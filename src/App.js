db759a6e97f9bf5260af01c3b3b41f01b87c8c0e
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-17 12:33:38 -0500

    re-arranged the buttons

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import "./App.css";
 
 function App() {
   let keypadArr = [
-    { display: "1", value: 1, type: "number" },
-    { display: "2", value: 2, type: "number" },
-    { display: "3", value: 3, type: "number" },
-    { display: "4", value: 4, type: "number" },
-    { display: "5", value: 5, type: "number" },
-    { display: "6", value: 6, type: "number" },
+    { display: "+", value: "plus", type: "operation" },
+    { display: "-", value: "minus", type: "operation" },
+    { display: "*", value: "multiply", type: "operation" },
+    { display: "/", value: "divide", type: "operation" },
     { display: "7", value: 7, type: "number" },
     { display: "8", value: 8, type: "number" },
     { display: "9", value: 9, type: "number" },
+    { display: "", value: "space", type: "space" },
+    { display: "4", value: 4, type: "number" },
+    { display: "5", value: 5, type: "number" },
+    { display: "6", value: 6, type: "number" },
+    { display: "", value: "space", type: "space" },
+    { display: "1", value: 1, type: "number" },
+    { display: "2", value: 2, type: "number" },
+    { display: "3", value: 3, type: "number" },
+    { display: "", value: "space", type: "space" },
+    { display: "C", value: "clear", type: "clear" },
     { display: "0", value: 1, type: "number" },
-    { display: "+", value: "plus", type: "operations" },
-    { display: "-", value: "minus", type: "operations" },
-    { display: "*", value: "multiply", type: "operations" },
-    { display: "-", value: "divide", type: "operations" },
-    { display: "=", value: "equals", type: "operations" },
+    { display: "=", value: "equals", type: "operation" },
   ];
 
   let buttonArr = keypadArr.map((button) => {
@@ -35,5 +39,8 @@ export default App;
 /*
 - create keypadArr to make an array of objects, for our buttons
 - map through all the buttons. want to show what the display is.
-
+- css for buttons to put into CSS Grid.
+- re-arrange the button objects. Can even add a button that's a space in between the buttons. OR grid areas (more complicated).
+- can go back later, delete the spaces, and re-arrange a different way.
+- add styling to the actual buttons themselves.
 */
