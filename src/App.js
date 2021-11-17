0b4055e27f76822379030406ad1585d570a855de
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-17 12:16:42 -0500

    createdg an array of objects for all the buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
 
 function App() {
+  let keypadArr = [
+    {display: "1", value: 1, type: "number"},
+    {display: "2", value: 2, type: "number"},
+    {display: "3", value: 3, type: "number"},
+    {display: "4", value: 4, type: "number"},
+    {display: "5", value: 5, type: "number"},
+    {display: "6", value: 6, type: "number"},
+    {display: "7", value: 7, type: "number"},
+    {display: "8", value: 8, type: "number"},
+    {display: "9", value: 9, type: "number"},
+    {display: "0", value: 1, type: "number"},
+    {display: "plus", value: "+", type: "operations"},
+    {display: "minus", value: "-", type: "operations"},
+    {display: "multiply", value: "*", type: "operations"},
+    {display: "divide", value: "/", type: "operations"},
+    {display: "equals", value: "=", type: "operations"},
+  ]
+
   return (
     <div>
     </div>
