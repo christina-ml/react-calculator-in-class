34f8d2b5c4edb69ec778eecd60cfdbe5afc32b86
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-17 12:23:33 -0500

    created all buttons and .map through each one

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   let keypadArr = [
-    {display: "1", value: 1, type: "number"},
-    {display: "2", value: 2, type: "number"},
-    {display: "3", value: 3, type: "number"},
-    {display: "4", value: 4, type: "number"},
-    {display: "5", value: 5, type: "number"},
-    {display: "6", value: 6, type: "number"},
-    {display: "7", value: 7, type: "number"},
-    {display: "8", value: 8, type: "number"},
-    {display: "9", value: 9, type: "number"},
-    {display: "0", value: 1, type: "number"},
-    {display: "plus", value: "+", type: "operations"},
-    {display: "minus", value: "-", type: "operations"},
-    {display: "multiply", value: "*", type: "operations"},
-    {display: "divide", value: "/", type: "operations"},
-    {display: "equals", value: "=", type: "operations"},
-  ]
+    { display: "1", value: 1, type: "number" },
+    { display: "2", value: 2, type: "number" },
+    { display: "3", value: 3, type: "number" },
+    { display: "4", value: 4, type: "number" },
+    { display: "5", value: 5, type: "number" },
+    { display: "6", value: 6, type: "number" },
+    { display: "7", value: 7, type: "number" },
+    { display: "8", value: 8, type: "number" },
+    { display: "9", value: 9, type: "number" },
+    { display: "0", value: 1, type: "number" },
+    { display: "+", value: "plus", type: "operations" },
+    { display: "-", value: "minus", type: "operations" },
+    { display: "*", value: "multiply", type: "operations" },
+    { display: "-", value: "divide", type: "operations" },
+    { display: "=", value: "equals", type: "operations" },
+  ];
 
-  return (
-    <div>
-    </div>
-  );
+  let buttonArr = keypadArr.map((button) => {
+    return <div className="calc-button">{button.display}</div>;
+  });
+
+  return <div>{buttonArr}</div>;
 }
 
 export default App;
+
+/*
+- create keypadArr to make an array of objects, for our buttons
+- map through all the buttons. want to show what the display is.
+
+*/
